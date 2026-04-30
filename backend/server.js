e2add2f795b1d43c4d8cf9d47798f6b7261b7b03
AryenMukundam <aryenmukundam@gmail.com>
2026-04-30 11:38:57 +0530

    Backend Service Deployed.

--- backend/server.js
+++ backend/server.js
@@ -49,5 +49,5 @@ app.get("/", (req, res) => {
   res.send("AI Chatbot is running.");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
